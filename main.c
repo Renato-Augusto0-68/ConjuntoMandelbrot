52efd37dbfd0559210ebc80213cf8b2974a7e305
Renato-Augusto0-68 <rass@cesar.school>
2026-08-27 15:35:05 -0300

    Refactor abrirarquivo function to accept valores and larg parameters for improved file writing

--- main.c
+++ main.c
@@ -5,32 +5,40 @@
 
 //Teste de mandelbrot na raça
 
-void abrirarquivo(int mode){
+void abrirarquivo(int mode, double *valores,int larg){
     FILE *arq;
-    
-
     if (mode==1)
-        arq = fopen("mandelbrot_rass_serial.pgm,","w");
+        arq = fopen("mandelbrot_rass_serial.pgm","w");
     if (mode==2)
         arq=fopen("mandelbrot_rass_openmp.pgm","w");
     if (mode==3)
-        arq = fopen("mandelbrot_rass_pthreads1.pgm,","w");
+        arq = fopen("mandelbrot_rass_pthreads1.pgm","w");
     if (mode==4)
         arq=fopen("mandelbrot_rass_pthreads2.pgm","w");
+    
+    if(arq!=NULL){
+        int cont =0;
+        while(valores!=NULL){
+            fprintf(arq,"%.2lf ",*valores);
+            if(cont==larg){
+                fprintf(arq,"\n");
+                cont = 0;
+            }else{
+                cont++;}
+            valores ++;
+        }
+    }
+    fclose(arq);
 }   
 
 int main(/*int argc, char *argv[]*/){
     int i=0;
-
-    
-    
-    
-    double total=10.0;
+    double total=50.0;
     double Z=0.0;
     double x1 = -2.0, x2=1.0;
     double y1 = -1.5,y2=1.5; 
-    int largura =6;
-    int altura =6;
+    int largura =4;
+    int altura =4;
     double *valores = (double *) malloc(sizeof(double)*largura*altura);    
     double x =(x2-x1)/largura; 
     double y = (y2-y1)/altura;
@@ -50,11 +58,13 @@ int main(/*int argc, char *argv[]*/){
                 interacoes++;
             }
             if(interacoes==total){
-                double valor = (interacoes/total)*255;
+                double valor = (interacoes/total)*255.0;
                 *valores = valor;
                 valores++;
             }
         }
+
+        abrirarquivo(1,valores,largura);
     }
     return 0;
 }
